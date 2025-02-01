05b492b5c65eaabb338656dc2ccc981b9e35bdf3
Piraj <59830750+Piraj@users.noreply.github.com>
2025-02-01 12:15:56 +0100

    added hud coin counter

--- scripts/gamemanager.gd
+++ scripts/gamemanager.gd
@@ -2,8 +2,10 @@ extends Node
 
 var score = 0
 @onready var score_label = $ScoreLabel
+@onready var score_hud_label: Label = $CanvasLayer/ScoreHUDLabel
 
 
 func add_point():
 	score += 1
 	score_label.text = "You collected " + str(score) + " coins."
+	score_hud_label.text = "Coins: " + str(score)
